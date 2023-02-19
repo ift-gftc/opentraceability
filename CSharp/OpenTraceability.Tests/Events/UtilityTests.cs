a0494a0886bffef0bc27fd063b6d57d18ca6930c
philip <philip.heggelund@duckscape.com>
2023-02-19 22:31:15 +0000

    Added unit test for JSON compare.

---
+++ CSharp/OpenTraceability.Tests/Events/UtilityTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace OpenTraceability.Tests.Events
+{
+	[TestFixture]
+	public class UtilityTests
+	{
+		[Test]
+		[TestCase("EPCISQueryDocument.jsonld")]
+		public void JSONCompareTest(string file)
+		{
+			string json1 = OpenTraceabilityTests.ReadTestData(file);
+			string json2 = OpenTraceabilityTests.ReadTestData(file);
+			OpenTraceabilityTests.CompareJSON(json1, json2);
+        }
+	}
+}
+
