2a25737befa527f92b1b0ecda0f2f570123eda34
Philip Heggelund <59456945+DuckScapePhilip@users.noreply.github.com>
2023-05-03 10:57:01 +0100

    Added MSC Storage Event.

--- CSharp/OpenTraceability.MSC/Setup.cs
+++ CSharp/OpenTraceability.MSC/Setup.cs
@@ -19,10 +19,15 @@ namespace OpenTraceability.MSC
 
                     OpenTraceability.Setup.RegisterEventProfile(new OpenTraceabilityEventProfile(typeof(MSCProcessingEvent), EventType.TransformationEvent, "urn:epcglobal:cbv:bizstep:commissioning"));
                     OpenTraceability.Setup.RegisterEventProfile(new OpenTraceabilityEventProfile(typeof(MSCProcessingEvent), EventType.TransformationEvent, "https://ref.gs1.org/cbv/BizStep-commissioning"));
+
                     OpenTraceability.Setup.RegisterEventProfile(new OpenTraceabilityEventProfile(typeof(MSCReceiveEvent), EventType.ObjectEvent, "urn:epcglobal:cbv:bizstep:receiving", Models.Events.EventAction.OBSERVE));
                     OpenTraceability.Setup.RegisterEventProfile(new OpenTraceabilityEventProfile(typeof(MSCReceiveEvent), EventType.ObjectEvent, "https://ref.gs1.org/cbv/BizStep-receiving", Models.Events.EventAction.OBSERVE));
+
                     OpenTraceability.Setup.RegisterEventProfile(new OpenTraceabilityEventProfile(typeof(MSCShippingEvent), EventType.ObjectEvent, "urn:epcglobal:cbv:bizstep:shipping", Models.Events.EventAction.OBSERVE));
                     OpenTraceability.Setup.RegisterEventProfile(new OpenTraceabilityEventProfile(typeof(MSCShippingEvent), EventType.ObjectEvent, "https://ref.gs1.org/cbv/BizStep-shipping", Models.Events.EventAction.OBSERVE));
+
+                    OpenTraceability.Setup.RegisterEventProfile(new OpenTraceabilityEventProfile(typeof(MSCStorageEvent), EventType.ObjectEvent, "urn:epcglobal:cbv:bizstep:storing", Models.Events.EventAction.OBSERVE));
+                    OpenTraceability.Setup.RegisterEventProfile(new OpenTraceabilityEventProfile(typeof(MSCStorageEvent), EventType.ObjectEvent, "https://ref.gs1.org/cbv/BizStep-storing", Models.Events.EventAction.OBSERVE));
 
                     _isInitialized = true;
                 }
