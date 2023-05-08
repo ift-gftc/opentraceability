2bceba132e5a08c88db4db315aa3eceec034c265
Philip Heggelund <59456945+DuckScapePhilip@users.noreply.github.com>
2023-05-08 15:54:44 +0100

    Added in dummy header.

--- CSharp/OpenTraceability/Models/Common/StandardBusinessDocumentHeader.cs
+++ CSharp/OpenTraceability/Models/Common/StandardBusinessDocumentHeader.cs
@@ -18,5 +18,42 @@ namespace OpenTraceability.Models.Common
         [OpenTraceabilityObject]
         [OpenTraceability(Constants.SBDH_NAMESPACE, "DocumentIdentification")]
         public SBDHDocumentIdentification? DocumentIdentification { get; set; }
+
+        public static StandardBusinessDocumentHeader DummyHeader
+        {
+            get
+            {
+                var dh = new StandardBusinessDocumentHeader();
+                dh.HeaderVersion = "x";
+                dh.Sender = new SBDHOrganization()
+                {
+                    Identifier = "x",
+                    ContactInformation = new SBDHContact()
+                    {
+                        ContactName = "x",
+                        EmailAddress = "x"
+                    }
+                };
+                dh.Receiver = new SBDHOrganization()
+                {
+                    Identifier = "x",
+                    ContactInformation = new SBDHContact()
+                    {
+                        ContactName = "x",
+                        EmailAddress = "x"
+                    }
+                };
+                dh.DocumentIdentification = new SBDHDocumentIdentification()
+                {
+                    CreationDateAndTime = DateTime.UtcNow,
+                    InstanceIdentifier = "x",
+                    MultipleType = "x",
+                    Standard = "x",
+                    Type = "x",
+                    TypeVersion = "x"
+                };
+                return dh;
+            }
+        }
     }
 }
