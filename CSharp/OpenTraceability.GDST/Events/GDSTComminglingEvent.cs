9f66270a35a502ceb78ca03107e35f4efe8c46da
Marcelo Nascimento <marcelo@wholechain.com>
2023-07-14 09:53:53 -0300

    Create GDSTComminglingEvent.cs

---
+++ CSharp/OpenTraceability.GDST/Events/GDSTComminglingEvent.cs
@@ -0,0 +1,13 @@
+using OpenTraceability.Models.Events;
+using OpenTraceability.Models.Identifiers;
+using OpenTraceability.Utility.Attributes;
+
+namespace OpenTraceability.GDST.Events
+{
+    public class GDSTComminglingEvent: TransformationEvent<GDSTILMD>, IGDSTILMDEvent   
+    {
+        [OpenTraceability(Constants.GDST_NAMESPACE, "productOwner")]
+        [OpenTraceabilityJson("gdst:productOwner")]
+        public PGLN? ProductOwner { get; set; }
+    }
+}
