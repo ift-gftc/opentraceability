81b67e93c068ff2e1b05ef75d709e02d801e689a
John Heggelund <heggelund.john@gmail.com>
2024-06-25 16:08:10 -0500

    add msc extensions project

---
+++ CSharp/OpenTraceability.MSC/Events/MSCProcessingEvent.cs
@@ -0,0 +1,22 @@
+﻿using OpenTraceability.Models.Events;
+using OpenTraceability.Models.Identifiers;
+using OpenTraceability.Utility.Attributes;
+
+namespace OpenTraceability.MSC.Events
+{
+    public class MSCProcessingEvent : TransformationEvent<MSCILMD>, IMSCILMDEvent
+    {
+        public MSCProcessingEvent()
+        {
+            this.BusinessStep = new Uri("urn:epcglobal:cbv:bizstep:commissioning");
+        }
+
+        [OpenTraceability(Constants.GDST_NAMESPACE, "productOwner")]
+        [OpenTraceabilityJson("gdst:productOwner")]
+        public PGLN? ProductOwner { get; set; }
+
+        [OpenTraceability(Constants.GDST_NAMESPACE, "humanWelfarePolicy")]
+        [OpenTraceabilityJson("gdst:humanWelfarePolicy")]
+        public string? HumanWelfarePolicy { get; set; }
+    }
+}
---
+++ CSharp/OpenTraceability.MSC/Events/MSCStorageEvent.cs
@@ -0,0 +1,24 @@
+﻿using OpenTraceability.Models.Events;
+using OpenTraceability.Models.Identifiers;
+using OpenTraceability.MSC.Events;
+using OpenTraceability.Utility.Attributes;
+
+namespace OpenTraceability.MSC.Events
+{
+    public class MSCStorageEvent : ObjectEvent<MSCILMD>, IMSCILMDEvent
+    {
+        public MSCStorageEvent()
+        {
+            this.BusinessStep = new Uri("urn:epcglobal:cbv:bizstep:storing");
+            this.Action = EventAction.OBSERVE;
+        }
+
+        [OpenTraceability(Constants.GDST_NAMESPACE, "productOwner")]
+        [OpenTraceabilityJson("gdst:productOwner")]
+        public PGLN? ProductOwner { get; set; }
+
+        [OpenTraceability(Constants.GDST_NAMESPACE, "humanWelfarePolicy")]
+        [OpenTraceabilityJson("gdst:humanWelfarePolicy")]
+        public string? HumanWelfarePolicy { get; set; }
+    }
+}
